db067ae6fdc828e432e592cd79c87ba8e8ba3d34
Strannik-chr <vaha.selmurz08@mail.ru>
2025-12-19 23:10:05 +0300

    Сделал сущность- model_season

---
+++ internal/models/season.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Season struct {
+	gorm.Model
+	Name      string     `json:"name" gorm:"not null"`
+	IsActive  bool       `json:"is_active" gorm:"default:false"`
+	StartDate *time.Time `json:"start_date"`
+	EndDate   *time.Time `json:"end_date"`
+
+	// Matches []Match             `gorm:"foreignKey:SeasonID"`
+	// Stats   []PlayerSeasonStats `gorm:"foreignKey:SeasonID"`
+}
