0c2bf5538be9b2d21bedd1df96191d03307dd37f
Alsiev Yusup <isaalsiev@gmail.com>
2025-12-19 16:29:21 +0300

    Сделал соединение и саму БД

--- cmd/shumnaya/main.go
+++ cmd/shumnaya/main.go
@@ -1,2 +1,20 @@
 package main
 
+import (
+	"log"
+
+	"shumnaya/internal/config"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found, using system environment variables")
+	}
+
+	config.ConnectDB()
+
+}
