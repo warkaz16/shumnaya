6784f8ac4f381442cc87b1425fff9834a02879d3
Alsiev Yusup <isaalsiev@gmail.com>
2025-12-21 21:47:31 +0300

    Сделал json-теги и binding для standig

--- internal/models/standing.go
+++ internal/models/standing.go
@@ -5,14 +5,14 @@ import "gorm.io/gorm"
 type Standing struct {
 	gorm.Model
 
-	PlayerID uint
-	Player   Player
+	PlayerID uint   `json:"player_id" binding:"required,min=1"`
+	Player   Player `json:"player"`
 
-	SeasonID uint
-	Season   Season
+	SeasonID uint   `json:"season_id" binding:"required,min=1"`
+	Season   Season `json:"season"`
 
-	Wins     int
-	Losses   int
-	Points   int
-	Rank     int
+	Wins   int `json:"wins" binding:"min=0"`
+	Losses int `json:"losses" binding:"min=0"`
+	Points int `json:"points" binding:"min=0"`
+	Rank   int `json:"rank" binding:"min=0"`
 }
