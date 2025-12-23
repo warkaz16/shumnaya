41c48d010e532c1bdc46c6b9298dea1d84439ae8
kuruevimran17 <ivahabov807@gmail.com>
2025-12-23 21:55:45 +0300

    исправил файл player-handler и добавил Dto структуру

--- internal/transport/player-handler.go
+++ internal/transport/player-handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"shumnaya/internal/dto"
 	"shumnaya/internal/service"
 
 	"github.com/gin-gonic/gin"
@@ -15,7 +16,7 @@ type PlayerHandler struct {
 	logger  *slog.Logger
 }
 
-func NewPlayerHandler(r *gin.Engine, svc service.PlayerService, logger *slog.Logger) *PlayerHandler {
+func NewPlayerHandler( svc service.PlayerService, logger *slog.Logger) *PlayerHandler {
 	return &PlayerHandler{service: svc, logger: logger}
 }
 
@@ -24,15 +25,7 @@ func (h *PlayerHandler) RegisterRoutes(r *gin.Engine) {
 	r.POST("/players", h.Register)
 }
 
-// GetByID godoc
-// @Summary Профиль игрока
-// @Tags Players
-// @Produce json
-// @Param id path int true "ID игрока"
-// @Success 200 {object} models.PlayerProfile
-// @Failure 400 {object} map[string]string
-// @Failure 500 {object} map[string]string
-// @Router /players/{id} [get]
+
 func (h *PlayerHandler) GetByID(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
@@ -52,28 +45,9 @@ func (h *PlayerHandler) GetByID(c *gin.Context) {
 	c.JSON(http.StatusOK, profile)
 }
 
-type RegisterPlayerRequest struct {
-	Name     string `json:"name" binding:"required" example:"Иван"`
-	Email    string `json:"email" binding:"required,email" example:"test@mail.com"`
-	Password string `json:"password" binding:"required,min=6" example:"secret123"`
-}
 
-type RegisterPlayerResponse struct {
-	Message string `json:"message" example:"игрок успешно зарегистрирован"`
-}
-
-// Register godoc
-// @Summary Регистрация игрока
-// @Description Создает нового игрока
-// @Tags Players
-// @Accept json
-// @Produce json
-// @Param input body RegisterPlayerRequest true "Данные регистрации"
-// @Success 201 {object} RegisterPlayerResponse
-// @Failure 400 {object} map[string]string
-// @Router /players [post]
 func (h *PlayerHandler) Register(c *gin.Context) {
-	var req RegisterPlayerRequest
+	var req dto.RegisterPlayerRequest
 
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(400, gin.H{
