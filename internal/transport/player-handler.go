2a4acdd0f191200d62e87afe18d465e546efaf0f
warkaz16 <adamaydamirov2018@gmail.com>
2025-12-23 16:22:49 +0300

    добавил функцию createPlayer в player-handler.go и swagger

--- internal/transport/player-handler.go
+++ internal/transport/player-handler.go
@@ -21,6 +21,7 @@ func NewPlayerHandler(r *gin.Engine, svc service.PlayerService, logger *slog.Log
 
 func (h *PlayerHandler) RegisterRoutes(r *gin.Engine) {
 	r.GET("/players/:id", h.GetByID)
+	r.POST("/players", h.Register)
 }
 
 // GetByID godoc
@@ -49,4 +50,57 @@ func (h *PlayerHandler) GetByID(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, profile)
+}
+
+type RegisterPlayerRequest struct {
+	Name     string `json:"name" binding:"required" example:"Иван"`
+	Email    string `json:"email" binding:"required,email" example:"test@mail.com"`
+	Password string `json:"password" binding:"required,min=6" example:"secret123"`
+}
+
+type RegisterPlayerResponse struct {
+	Message string `json:"message" example:"игрок успешно зарегистрирован"`
+}
+
+// Register godoc
+// @Summary Регистрация игрока
+// @Description Создает нового игрока
+// @Tags Players
+// @Accept json
+// @Produce json
+// @Param input body RegisterPlayerRequest true "Данные регистрации"
+// @Success 201 {object} RegisterPlayerResponse
+// @Failure 400 {object} map[string]string
+// @Router /players [post]
+func (h *PlayerHandler) Register(c *gin.Context) {
+	var req RegisterPlayerRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{
+			"error": "некорректные данные регистрации",
+		})
+		return
+	}
+
+	if err := h.service.RegisterPlayer(
+		req.Name,
+		req.Email,
+		req.Password,
+	); err != nil {
+		if h.logger != nil {
+			h.logger.Error(
+				"handler: ошибка регистрации игрока",
+				"email", req.Email,
+				"error", err,
+			)
+		}
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(201, gin.H{
+		"message": "игрок успешно зарегистрирован",
+	})
 }
